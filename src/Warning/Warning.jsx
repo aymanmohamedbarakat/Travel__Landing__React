8b1d26103c7eab8364e729a763e62f8015751e06
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-02-14 20:13:46 +0200

    last-commit

--- src/Warning/Warning.jsx
+++ src/Warning/Warning.jsx
@@ -3,7 +3,7 @@ import { PiWarning } from "react-icons/pi";
 export default function Warning(){
     return (
         <div className="warning p-0">
-            <div className="Warning__container bg-warning py-3 rounded-1">
+            <div className="Warning__container bg-warning py-4 rounded-1">
                 <div className="Warning__content d-flex align-items-center gap-3 px-3 ">
                     <span><PiWarning size={30} /></span>
                     <p>Check the latest COVID-19 restrictions before you travel. <a href="#" className="">Learn more</a> </p>
